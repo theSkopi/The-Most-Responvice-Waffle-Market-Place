b13341eff9540569976cd10e0d72eec98e9b8bb9
theSkopi <matas.ouba@seznam.cz>
2022-05-21 00:01:11 +0200

    Header design 2.0

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -27,85 +27,79 @@ function Header({
   };
 
   return (
-    <header className={`${Styles.header} ${theme}`}>
-      <div className={Styles.logo__and__theme}>
-        <Link to={"/"}>
-          <img
-            className={Styles.logo}
-            src={process.env.PUBLIC_URL + "images/waffles-logo.png"}
-            alt="waffle shuffle logo"
-          />
-          <img
-            className={Styles.logo__mini}
-            src={process.env.PUBLIC_URL + "images/logo-mini.png"}
-            alt="waffle shuffle logo"
-          />
-        </Link>
-        <ul className={Styles.theme__switch}>
-          <button
-            className={themeState === "light" ? Styles.selected : ""}
-            onClick={() => handleThemeSwitch("light")}
-          >
-            <BsSun />
-          </button>
-          <button
-            className={themeState === "dark" ? Styles.selected : ""}
-            onClick={() => handleThemeSwitch("dark")}
-          >
-            <MdDarkMode />
-          </button>
-          <button
-            className={themeState === "auto" ? Styles.selected : ""}
-            onClick={() => handleThemeSwitch("auto")}
-          >
-            <BsClockHistory />
-          </button>
-        </ul>
-      </div>
+    <header className={theme}>
+      {/* <div className={Styles.logo__and__theme}> */}
+      <Link to={"/"}>
+        <img
+          className={Styles.logo}
+          src={process.env.PUBLIC_URL + "images/waffles-logo.png"}
+          alt="waffle shuffle logo"
+        />
+        <img
+          className={Styles.logo__mini}
+          src={process.env.PUBLIC_URL + "images/logo-mini.png"}
+          alt="waffle shuffle logo"
+        />
+      </Link>
+      {/* </div> */}
       <ul className={Styles.menu__container}>
-        <li>
+        <li className={pathName === "/" ? Styles.current__site : ""}>
           {" "}
-          <Link
-            className={pathName === "/" ? Styles.current__site : ""}
-            to={"/"}
-          >
+          <Link to={"/"}>
             <BiHomeAlt className={Styles.menu__icon} />
             <h5>HOME</h5>
           </Link>
         </li>
-        <li>
-          <Link
-            className={
-              pathName === "/order" ? Styles.current__site : ""
-            }
-            to={"/order"}
-          >
+        <li
+          className={
+            pathName === "/order" ? Styles.current__site : ""
+          }
+        >
+          <Link to={"/order"}>
             <HiOutlineNewspaper className={Styles.menu__icon} />
             <h5>ORDER</h5>
           </Link>
         </li>
-        <li>
-          <Link
-            className={
-              pathName === "/waffles" ? Styles.current__site : ""
-            }
-            to={"/waffles"}
-          >
+        <li
+          className={
+            pathName === "/waffles" ? Styles.current__site : ""
+          }
+        >
+          <Link to={"/waffles"}>
             <MdOutlineFastfood className={Styles.menu__icon} />
             <h5>WAFFLES</h5>
           </Link>
         </li>
-        <li>
-          <Link
-            className={
-              pathName === "/join-us" ? Styles.current__site : ""
-            }
-            to={"/join-us"}
-          >
+        <li
+          className={
+            pathName === "/join-us" ? Styles.current__site : ""
+          }
+        >
+          <Link to={"/join-us"}>
             <AiOutlinePlusCircle className={Styles.menu__icon} />
             <h5>JOIN US!</h5>
           </Link>
         </li>
+      </ul>
+      <ul className={Styles.theme__switch}>
+        <button
+          className={themeState === "light" ? Styles.selected : ""}
+          onClick={() => handleThemeSwitch("light")}
+        >
+          <BsSun />
+        </button>
+        <button
+          className={themeState === "dark" ? Styles.selected : ""}
+          onClick={() => handleThemeSwitch("dark")}
+        >
+          <MdDarkMode />
+        </button>
+        <button
+          className={themeState === "auto" ? Styles.selected : ""}
+          onClick={() => handleThemeSwitch("auto")}
+        >
+          <BsClockHistory />
+        </button>
       </ul>
     </header>
   );
